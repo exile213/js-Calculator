e9a476fb090f9570ff54086c72e38e4c8de332f3
exile213 <exilegamer3@gmail.com>
2024-03-16 10:27:47 +0800

    Link JS to html file, Testing operation functions

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(num1,num2){
+    return num1+num2;
+}
+function subtract(num1,num2){
+    return num1-num2;
+}
+function multiply(num1,num2){
+    return num1*num2;
+}
+function divide(num1,num2){
+    return num1/num2;
+}
+
+console.log(add(21,33));
+console.log(subtract(45,23));
+console.log(multiply(23,84));
+console.log(divide(55,5));
