de6105b85a7db3e490b51f99d02edd8c54893bec
exile213 <exilegamer3@gmail.com>
2024-04-26 22:03:25 +0800

    Update stateVariables to String, Update Number Input collection to Array

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 //VARIABLES
-let numbers=[];
-let num1;
-let num2;
+let numArray=[];
 let operator = '';
 let displayVal="";
 
 //STATE VARIABLES
+let state="firstNum";
 let resNum = false;
-let inputNum = 0;
 
 //HTML ELEMENT VARIABLES
 let inputText = document.getElementById('inputText');
@@ -15,34 +13,49 @@ let numButton = document.querySelectorAll('.buttons');
 let opButton = document.querySelectorAll('.op-buttons');
 
 
-//EVENT LISTENERS
+//NUMBERS EVENT LISTENERS
     for(let i=0; i<numButton.length;i++){
         numButton[i].addEventListener('click',function () {
+
             if(resNum){
                 displayVal ="";
                 inputText.textContent ="";
                 resNum = false;
             }
+            if(state=="firstNum"){
                 displayVal += numButton[i].value;
                 inputText.textContent = displayVal;
-                numbers.push(displayVal);
-                inputNum++;
+            }
+
+            if(state=="secondNum"){
+                displayVal += numButton[i].value;
+                inputText.textContent = displayVal;
+            }
        });
     }
 
+
+    //OPERATION BUTTONS EVENT LISTENERS
     for(let i=0; i<opButton.length;i++){
         opButton[i].addEventListener('click',function () {
-            if(inputNum ==1){
+            if(state=="firstNum"){
+                numArray.push(displayVal);
                 displayVal = opButton[i].value;
+                operator = opButton[i].value;
                 inputText.textContent = displayVal;
-                operator = displayVal;
-                inputNum++;
+                state="secondNum";
                 resNum=true;
+                numArray.push(displayVal);
             }
-            if(inputNum==3){
-                alert(num1,num2,operator);
+
+            if(state=="secondNum" && resNum ==false){
+                numArray.push(displayVal);
+                alert(numArray);
                 displayVal = opButton[i].value;
+                operator = opButton[i].value;
                 inputText.textContent = displayVal;
+                state ="firstNum";
+                numArray.push(displayVal);
 
             }
        });
@@ -82,3 +95,4 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1/num2;
 }
+ 
