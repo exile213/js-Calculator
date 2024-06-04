7d86966590c1b0e6a847aae7e58a1e2311338f49
exile213 <exilegamer3@gmail.com>
2024-06-04 11:20:58 +0800

    Add Display Previous Inputs Functionality
    Add nextrun State Variable
    Add did Equals State Variable
    Add prevInput Variable
    
    Add PrevText Creation Functionality
    
    Add PrevText styles

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //VARIABLES
 let num1,num2;
 let operator = '';
-let displayArray=[];
+let displayArray=[132123];
 let displayVal="";
 let result;
+let prevInput;
 
 //STATE VARIABLES
 let state=0;
 let resNum = false;
 let oneDeci = false;
+let nextRun = false;
+let didEquals = false;
 
 //HTML ELEMENT VARIABLES
 let inputText = document.getElementById('inputText');
@@ -63,8 +66,10 @@ document.addEventListener('keydown', (event) => {
          inputText.textContent ='';
          displayArray=[];
          displayVal="";
+         prevInput.textContent ="";
          state=0;
          resNum = false;
+         nextRun = false;
     });
 
 //DELETE BUTTON EVENT LISTENER
@@ -85,11 +90,17 @@ document.addEventListener('keydown', (event) => {
 function numButtonInput(value){
     if (resNum) {
         operator = displayVal;
+        displayArray[1] = operator;
         displayVal = '';
         inputText.textContent = '';
         resNum = false;
         oneDeci = false;
+        didEquals=false;
         state = 3;
+        if(nextRun==true){
+          displayArray[0] =num1;
+          displayArray[2] =""; 
+        }
       }
       if (state == 0) {
         state = 1;
@@ -120,6 +131,7 @@ function numButtonInput(value){
           displayVal += value;
           inputText.textContent = displayVal;
         }
+        prevInput.textContent = displayArray.join(" ");
       }
 }
 
@@ -127,6 +139,10 @@ function numButtonInput(value){
 function opButtonInput(value){
     if (state == 1) {
         num1 = displayVal;
+        displayArray[0] = num1;
+        prevInput = document.createElement('div');
+        prevInput.classList.add('prevText');
+      
         state = 2;
       }
       if (state == 2) {
@@ -134,19 +150,31 @@ function opButtonInput(value){
         operator = value;
         inputText.textContent = displayVal;
         resNum = true;
+
+      
+        prevInput.textContent = displayArray.join(' ');
+        // Insert the prevInput element before the inputText element
+        inputText.insertAdjacentElement('beforebegin', prevInput);
       }
     
       if (state == 3) {
-        if (!resNum) {
+        if (!resNum && didEquals ==false) {
           num2 = displayVal;
+          displayArray[2] = num2;
+          prevInput.textContent = displayArray.join(" ");
           num1 = operate(num1, num2, operator);
           inputText.textContent = num1;
         }
-        if (value !== '=') {
+        if(value =='='){
+          didEquals = true;
+        }
+        if (value !== '=' ) {
           displayVal = value;
           operator = value;
           resNum = true;
+          nextRun = true;
         }
+
       }
 }
 
@@ -191,8 +219,8 @@ function multiply(num1,num2){
 }
 function divide(num1,num2){
     if(num2 ==0){
-        alert("Nice try dumboo, you cant by  0");
-        return"RESTART, DONT TRY DIVING BY 0";
+        alert("Nice try dumboo, you cant divide by  0");
+        return"RESTART, DONT TRY DIVIDING BY 0";
     }else{
         result = parseFloat(num1)/parseFloat(num2);
         return result.toFixed(2);
